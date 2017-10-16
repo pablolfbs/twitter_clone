da5e5ca497b8a877f28d9e5d45eda4227e529037
Pablo <pablo.fariabritto@gmail.com>
2017-10-16 14:54:35 -0200

    Iniciando repositório twitter_clone

---
+++ db.class.php
@@ -0,0 +1,27 @@
+<?php
+
+class db {
+
+	// host
+	private $host = 'localhost';
+
+	// usuario
+	private $usuario = 'root';
+
+	// senha
+	private $senha = '';
+
+	// banco de dados
+	private $database = 'twitter_clone';
+
+	public function conecta_mysql() {
+		$con = mysqli_connect($this->host, $this->usuario, $this->senha, $this->database);
+		mysqli_set_charset($con, 'utf8');
+		if (mysqli_connect_errno()) {
+			echo 'Erro ao tentar se conectar com o BD MySQL'.mysqli_connect_error();
+		}
+		return $con;
+	}
+}
+
+?>
